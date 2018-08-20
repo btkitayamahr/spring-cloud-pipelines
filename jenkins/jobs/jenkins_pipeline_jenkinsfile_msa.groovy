df900155881d5d5e00985f54db9eb788c8ff6288
btiwanagah <iwanagah@pm.nttdata.co.jp>
2018-08-20 17:56:31 +0900

    modify variables

--- jenkins/jobs/jenkins_pipeline_jenkinsfile_msa.groovy
+++ jenkins/jobs/jenkins_pipeline_jenkinsfile_msa.groovy
@@ -12,6 +12,12 @@ DslFactory dsl = this
 String jenkinsfileDir = binding.variables["JENKINSFILE_DIR"] ?: "${WORKSPACE}/jenkins/declarative-pipeline"
 
 dsl.pipelineJob('msa-pipline-discovery') {
+	wrappers {
+		parameters {
+			stringParam('APP_REPOSITORY', 'https://terasoluna-cloud-management-423625999.ap-northeast-1.elb.amazonaws.com/git/terasoluna-msa/verification.git', "The URL with application repository. Also git repository")
+			stringParam('APP_BRANCH', 'master', "The branch with application")
+		}
+	}
 	definition {
 		cps {
 			script("""${dsl.readFileFromWorkspace(jenkinsfileDir + '/Jenkinsfile-discovery')}""")
