678ed23d187009245cf84ed54d10324b74187b68
btkitayamahr <kitayamahr@pm.nttdata.co.jp>
2018-08-28 14:55:41 +0900

    modify

--- jenkins/jobs/jenkins_pipeline_jenkinsfile_msa.groovy
+++ jenkins/jobs/jenkins_pipeline_jenkinsfile_msa.groovy
@@ -16,6 +16,7 @@ dsl.pipelineJob('msa-pipeline-discovery') {
 		parameters {
 			stringParam('GIT_REPOSITORY', 'https://terasoluna-cloud-management-423625999.ap-northeast-1.elb.amazonaws.com/git/terasoluna-msa/verification.git', "The URL with application repository. Also git repository")
 			stringParam('GIT_BRANCH_NAME', 'master', "The branch with application")
+			stringParam('GIT_CREDENTIAL_ID', 'git-gitbucket', "")
 			stringParam('SSH_CONFIG_NAME_TEST', 'msa-ap-test', "Setting name of ssh connection")
 			stringParam('DOCKER_REGISTRY_HOSTNAME', '13.112.188.210', "")
 			stringParam('DOCKER_REGISTRY_PORT', '5000', "")
@@ -33,6 +34,7 @@ dsl.pipelineJob('msa-pipeline-gateway') {
 		parameters {
 			stringParam('GIT_REPOSITORY', 'https://terasoluna-cloud-management-423625999.ap-northeast-1.elb.amazonaws.com/git/terasoluna-msa/verification.git', "The URL with application repository. Also git repository")
 			stringParam('GIT_BRANCH_NAME', 'master', "The branch with application")
+			stringParam('GIT_CREDENTIAL_ID', 'git-gitbucket', "")
 			stringParam('SSH_CONFIG_NAME_TEST', 'msa-ap-test', "Setting name of ssh connection")
 			stringParam('DOCKER_REGISTRY_HOSTNAME', '13.112.188.210', "")
 			stringParam('DOCKER_REGISTRY_PORT', '5000', "")
@@ -50,6 +52,7 @@ dsl.pipelineJob('msa-pipeline-config') {
 		parameters {
 			stringParam('GIT_REPOSITORY', 'https://terasoluna-cloud-management-423625999.ap-northeast-1.elb.amazonaws.com/git/terasoluna-msa/verification.git', "The URL with application repository. Also git repository")
 			stringParam('GIT_BRANCH_NAME', 'master', "The branch with application")
+			stringParam('GIT_CREDENTIAL_ID', 'git-gitbucket', "")
 			stringParam('SSH_CONFIG_NAME_TEST', 'msa-ap-test', "Setting name of ssh connection")
 			stringParam('DOCKER_REGISTRY_HOSTNAME', '13.112.188.210', "")
 			stringParam('DOCKER_REGISTRY_PORT', '5000', "")
@@ -67,6 +70,7 @@ dsl.pipelineJob('msa-pipeline-contents') {
 		parameters {
 			stringParam('GIT_REPOSITORY', 'https://terasoluna-cloud-management-423625999.ap-northeast-1.elb.amazonaws.com/git/terasoluna-msa/verification.git', "The URL with application repository. Also git repository")
 			stringParam('GIT_BRANCH_NAME', 'master', "The branch with application")
+			stringParam('GIT_CREDENTIAL_ID', 'git-gitbucket', "")
 			stringParam('SSH_CONFIG_NAME_TEST', 'msa-ap-test', "Setting name of ssh connection")
 			stringParam('DOCKER_REGISTRY_HOSTNAME', '13.112.188.210', "")
 			stringParam('DOCKER_REGISTRY_PORT', '5000', "")
@@ -85,6 +89,7 @@ dsl.pipelineJob('msa-pipeline-frontend') {
 		parameters {
 			stringParam('GIT_REPOSITORY', 'https://terasoluna-cloud-management-423625999.ap-northeast-1.elb.amazonaws.com/git/terasoluna-msa/verification.git', "The URL with application repository. Also git repository")
 			stringParam('GIT_BRANCH_NAME', 'master', "The branch with application")
+			stringParam('GIT_CREDENTIAL_ID', 'git-gitbucket', "")
 			stringParam('SSH_CONFIG_NAME_TEST', 'msa-ap-test', "Setting name of ssh connection")
 			stringParam('DOCKER_REGISTRY_HOSTNAME', '13.112.188.210', "")
 			stringParam('DOCKER_REGISTRY_PORT', '5000', "")
