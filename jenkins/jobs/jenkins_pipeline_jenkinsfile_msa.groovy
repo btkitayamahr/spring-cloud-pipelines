a2a3392f3a87932f9854ba6b0201d4fd5d3c55de
btkitayamahr <kitayamahr@pm.nttdata.co.jp>
2018-08-30 11:41:47 +0900

    modify

--- jenkins/jobs/jenkins_pipeline_jenkinsfile_msa.groovy
+++ jenkins/jobs/jenkins_pipeline_jenkinsfile_msa.groovy
@@ -21,13 +21,8 @@ envs['TOOLS_BRANCH'] = binding.variables['TOOLS_BRANCH']
 envs['SSH_CONFIG_NAME_TEST'] = 'msa-ap-test'
 envs['SSH_CONFIG_NAME_PROD'] = 'msa-ap-prod'
 
-// envs['HOSTNAME_TEST'] = '13.115.229.114'
-// envs['DOCKER_REGISTRY_HOSTNAME'] = '13.112.188.210'
-// envs['DOCKER_REGISTRY_PORT'] = '5000'
-
 dsl.pipelineJob('msa-pipeline-config') {
 	envs['APP_NAME'] = 'config'
-	envs['DOCKER_COMPOSE_YML_TEST'] = 'docker-compose-config-test.yml'
 
 	wrappers {
 		parameters {
@@ -52,7 +47,6 @@ dsl.pipelineJob('msa-pipeline-config') {
 
 dsl.pipelineJob('msa-pipeline-discovery') {
 	envs['APP_NAME'] = 'discovery'
-	envs['DOCKER_COMPOSE_YML_TEST'] = 'docker-compose-discovery-test.yml'
 
 	wrappers {
 		parameters {
@@ -77,7 +71,6 @@ dsl.pipelineJob('msa-pipeline-discovery') {
 
 dsl.pipelineJob('msa-pipeline-frontend') {
 	envs['APP_NAME'] = 'frontend'
-	envs['DOCKER_COMPOSE_YML_TEST'] = 'docker-compose-frontend-test.yml'
 
 	wrappers {
 		parameters {
@@ -102,7 +95,6 @@ dsl.pipelineJob('msa-pipeline-frontend') {
 
 dsl.pipelineJob('msa-pipeline-contents') {
 	envs['APP_NAME'] = 'contents'
-	envs['DOCKER_COMPOSE_YML_TEST'] = 'docker-compose-contents-test.yml'
 
 	wrappers {
 		parameters {
@@ -127,7 +119,6 @@ dsl.pipelineJob('msa-pipeline-contents') {
 
 dsl.pipelineJob('msa-pipeline-gateway') {
 	envs['APP_NAME'] = 'gateway'
-	envs['DOCKER_COMPOSE_YML_TEST'] = 'docker-compose-gateway-test.yml'
 
 	wrappers {
 		parameters {
