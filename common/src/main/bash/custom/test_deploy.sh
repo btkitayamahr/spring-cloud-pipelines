d45e8f1892b85688623a65692e6bf254b82db2a5
btkitayamahr <kitayamahr@pm.nttadata.co.jp>
2018-09-05 15:09:36 +0900

    modify

--- common/src/main/bash/custom/test_deploy.sh
+++ common/src/main/bash/custom/test_deploy.sh
@@ -2,6 +2,6 @@
 
 function testDeploy() {
     echo "I am executing a custom test deploy function"
-    ssh -i ../.ssh/id_rsa centos@52.198.239.61
+    ssh -i .ssh/id_rsa centos@52.198.239.61
     ls
 }
