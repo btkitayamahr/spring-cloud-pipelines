61f198a925cbea4628199de3186b11e3e3dcc5b6
btkitayamahr <kitayamahr@pm.nttadata.co.jp>
2018-09-05 20:05:46 +0900

    modify

---
+++ common/src/main/bash/custom/test_smoke.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+function prepareForSmokeTests() {
+    echo "I am executing a custom prepare for smoke tests function"
+}
+
+function runSmokeTests() {
+    echo "I am executing a custom run smoke tests function"
+}
