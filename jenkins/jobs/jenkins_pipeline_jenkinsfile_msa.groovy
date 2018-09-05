a120de3195e4fba7daa20c815f97de420e6c8088
btkitayamahr <kitayamahr@pm.nttadata.co.jp>
2018-09-05 14:55:11 +0900

    modify

--- jenkins/jobs/jenkins_pipeline_jenkinsfile_msa.groovy
+++ jenkins/jobs/jenkins_pipeline_jenkinsfile_msa.groovy
@@ -20,9 +20,9 @@ envs['GIT_BRANCH_NAME'] = 'scc'
 envs['TOOLS_REPOSITORY'] = binding.variables["TOOLS_REPOSITORY"]
 envs['TOOLS_BRANCH'] = binding.variables["TOOLS_BRANCH"]
 envs['GIT_CREDENTIAL_ID'] = 'git-gitbucket'
-// envs['CONFIG_REPOSITORY'] = 'https://terasoluna-cloud-management-423625999.ap-northeast-1.elb.amazonaws.com/git/terasoluna-msa/verification.git'
-// envs['CONFIG_BRANCH_NAME'] = 'scc'
-// envs['CONFIG_CREDENTIAL_ID'] = 'git-gitbucket'
+envs['CONFIG_REPOSITORY'] = 'https://terasoluna-cloud-management-423625999.ap-northeast-1.elb.amazonaws.com/git/terasoluna-msa/verification.git'
+envs['CONFIG_BRANCH'] = 'scc'
+envs['CONFIG_CREDENTIAL_ID'] = 'git-gitbucket'
 
 envs['SSH_CONFIG_NAME_TEST'] = 'msa-ap-test'
 envs['SSH_CONFIG_NAME_PROD'] = 'msa-ap-prod'
@@ -37,9 +37,9 @@ dsl.pipelineJob('msa-pipeline-config') {
 			stringParam('GIT_CREDENTIAL_ID', envs['GIT_CREDENTIAL_ID'], "")
 			stringParam('TOOLS_REPOSITORY', envs['TOOLS_REPOSITORY'], "")
 			stringParam('TOOLS_BRANCH', envs['TOOLS_BRANCH'], "")
-//			stringParam('CONFIG_REPOSITORY', envs['CONFIG_REPOSITORY'], "")
-//			stringParam('CONFIG_BRANCH_NAME', envs['CONFIG_BRANCH_NAME'], "")
-//			stringParam('CONFIG_CREDENTIAL_ID', envs['CONFIG_CREDENTIAL_ID'], "")
+			stringParam('CONFIG_REPOSITORY', envs['CONFIG_REPOSITORY'], "")
+			stringParam('CONFIG_BRANCH', envs['CONFIG_BRANCH'], "")
+			stringParam('CONFIG_CREDENTIAL_ID', envs['CONFIG_CREDENTIAL_ID'], "")
 			stringParam('SSH_CONFIG_NAME_TEST', envs['SSH_CONFIG_NAME_TEST'], "")
 			stringParam('SSH_CONFIG_NAME_PROD', envs['SSH_CONFIG_NAME_PROD'], "")
 		}
@@ -64,9 +64,9 @@ dsl.pipelineJob('msa-pipeline-discovery') {
 			stringParam('GIT_CREDENTIAL_ID', envs['GIT_CREDENTIAL_ID'], "")
 			stringParam('TOOLS_REPOSITORY', envs['TOOLS_REPOSITORY'], "")
 			stringParam('TOOLS_BRANCH', envs['TOOLS_BRANCH'], "")
-//			stringParam('CONFIG_REPOSITORY', envs['CONFIG_REPOSITORY'], "")
-//			stringParam('CONFIG_BRANCH_NAME', envs['CONFIG_BRANCH_NAME'], "")
-//			stringParam('CONFIG_CREDENTIAL_ID', envs['CONFIG_CREDENTIAL_ID'], "")
+			stringParam('CONFIG_REPOSITORY', envs['CONFIG_REPOSITORY'], "")
+			stringParam('CONFIG_BRANCH', envs['CONFIG_BRANCH'], "")
+			stringParam('CONFIG_CREDENTIAL_ID', envs['CONFIG_CREDENTIAL_ID'], "")
 			stringParam('SSH_CONFIG_NAME_TEST', envs['SSH_CONFIG_NAME_TEST'], "")
 			stringParam('SSH_CONFIG_NAME_PROD', envs['SSH_CONFIG_NAME_PROD'], "")
 		}
@@ -91,9 +91,9 @@ dsl.pipelineJob('msa-pipeline-frontend') {
 			stringParam('GIT_CREDENTIAL_ID', envs['GIT_CREDENTIAL_ID'], "")
 			stringParam('TOOLS_REPOSITORY', envs['TOOLS_REPOSITORY'], "")
 			stringParam('TOOLS_BRANCH', envs['TOOLS_BRANCH'], "")
-//			stringParam('CONFIG_REPOSITORY', envs['CONFIG_REPOSITORY'], "")
-//			stringParam('CONFIG_BRANCH_NAME', envs['CONFIG_BRANCH_NAME'], "")
-//			stringParam('CONFIG_CREDENTIAL_ID', envs['CONFIG_CREDENTIAL_ID'], "")
+			stringParam('CONFIG_REPOSITORY', envs['CONFIG_REPOSITORY'], "")
+			stringParam('CONFIG_BRANCH', envs['CONFIG_BRANCH'], "")
+			stringParam('CONFIG_CREDENTIAL_ID', envs['CONFIG_CREDENTIAL_ID'], "")
 			stringParam('SSH_CONFIG_NAME_TEST', envs['SSH_CONFIG_NAME_TEST'], "")
 			stringParam('SSH_CONFIG_NAME_PROD', envs['SSH_CONFIG_NAME_PROD'], "")
 		}
@@ -118,9 +118,9 @@ dsl.pipelineJob('msa-pipeline-contents') {
 			stringParam('GIT_CREDENTIAL_ID', envs['GIT_CREDENTIAL_ID'], "")
 			stringParam('TOOLS_REPOSITORY', envs['TOOLS_REPOSITORY'], "")
 			stringParam('TOOLS_BRANCH', envs['TOOLS_BRANCH'], "")
-//			stringParam('CONFIG_REPOSITORY', envs['CONFIG_REPOSITORY'], "")
-//			stringParam('CONFIG_BRANCH_NAME', envs['CONFIG_BRANCH_NAME'], "")
-//			stringParam('CONFIG_CREDENTIAL_ID', envs['CONFIG_CREDENTIAL_ID'], "")
+			stringParam('CONFIG_REPOSITORY', envs['CONFIG_REPOSITORY'], "")
+			stringParam('CONFIG_BRANCH', envs['CONFIG_BRANCH'], "")
+			stringParam('CONFIG_CREDENTIAL_ID', envs['CONFIG_CREDENTIAL_ID'], "")
 			stringParam('SSH_CONFIG_NAME_TEST', envs['SSH_CONFIG_NAME_TEST'], "")
 			stringParam('SSH_CONFIG_NAME_PROD', envs['SSH_CONFIG_NAME_PROD'], "")
 		}
@@ -145,9 +145,9 @@ dsl.pipelineJob('msa-pipeline-gateway') {
 			stringParam('GIT_CREDENTIAL_ID', envs['GIT_CREDENTIAL_ID'], "")
 			stringParam('TOOLS_REPOSITORY', envs['TOOLS_REPOSITORY'], "")
 			stringParam('TOOLS_BRANCH', envs['TOOLS_BRANCH'], "")
-//			stringParam('CONFIG_REPOSITORY', envs['CONFIG_REPOSITORY'], "")
-//			stringParam('CONFIG_BRANCH_NAME', envs['CONFIG_BRANCH_NAME'], "")
-//			stringParam('CONFIG_CREDENTIAL_ID', envs['CONFIG_CREDENTIAL_ID'], "")
+			stringParam('CONFIG_REPOSITORY', envs['CONFIG_REPOSITORY'], "")
+			stringParam('CONFIG_BRANCH', envs['CONFIG_BRANCH'], "")
+			stringParam('CONFIG_CREDENTIAL_ID', envs['CONFIG_CREDENTIAL_ID'], "")
 			stringParam('SSH_CONFIG_NAME_TEST', envs['SSH_CONFIG_NAME_TEST'], "")
 			stringParam('SSH_CONFIG_NAME_PROD', envs['SSH_CONFIG_NAME_PROD'], "")
 		}
