d9697ba4cf8f8e90c3be8d7f271b15684f9490da
btkitayamahr <kitayamahr@pm.nttadata.co.jp>
2018-09-05 16:13:31 +0900

    modify

--- common/src/main/bash/custom/test_deploy.sh
+++ common/src/main/bash/custom/test_deploy.sh
@@ -5,6 +5,6 @@ function testDeploy() {
     ssh -i ${JENKINS_HOME}/.ssh/id_rsa ${USERNAME_TEST}@${HOSTNAME_TEST} sudo docker stop ${APP_NAME}
     ssh -i ${JENKINS_HOME}/.ssh/id_rsa ${USERNAME_TEST}@${HOSTNAME_TEST} sudo docker rm ${APP_NAME}
     ssh -i ${JENKINS_HOME}/.ssh/id_rsa ${USERNAME_TEST}@${HOSTNAME_TEST} sudo docker image prune -a -f
-    scp -i ${JENKINS_HOME}/.ssh/id_rsa ${WORKSPACE}@config/config-repos/${APP_NAME}/docker/docker-compose-${APP_NAME}-test.yml ${USERNAME_TEST}@${HOSTNAME_TEST}:/home/centos
+    scp -i ${JENKINS_HOME}/.ssh/id_rsa ${WORKSPACE}@config/config-repo/${APP_NAME}/docker/docker-compose-${APP_NAME}-test.yml ${USERNAME_TEST}@${HOSTNAME_TEST}:/home/centos
     ssh -i ${JENKINS_HOME}/.ssh/id_rsa ${USERNAME_TEST}@${HOSTNAME_TEST} sudo docker-compose -f docker-compose-${APP_NAME}-test.yml up -d
 }
